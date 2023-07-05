8b3dc2fcfb11873fb164a422323115a61ac7635f
monaabishek2003 <mona2010898@ssn.edu.in>
2023-07-05 21:23:24 +0530

    update3

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Stats from "./Stats";
 import Aboutus from "./Aboutus";
-import Services from "./services";
+import Services from "./Services";
 import Testimonials from "./Testimonials";
 import CTA from "./CTA";
 import Contact from "./Contact";
